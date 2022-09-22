20c0051a5a76176dece211f61760da323b0cae58
ravi <ravi.chaudhari@apaarinfosystems.com>
2022-09-22 16:56:50 +0530

    add outward apis

--- WMS_WebAPI/WMS_WebAPI/Controllers/OutwardController.cs
+++ WMS_WebAPI/WMS_WebAPI/Controllers/OutwardController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using WMS_WebAPI.Models;
 using WMS_WebAPI.Models.Context;
@@ -78,22 +81,64 @@ namespace WMS_WebAPI.Controllers
 
         [HttpPost]
         [Route("api/Outward/Outward_Insert")]
-        public IHttpActionResult Outward_Insert(cls_Outward obj)
+        public IHttpActionResult Outward_Insert([FromBody]OutwardSaveModel osm)
         {
-            try
+            DataTable dtOutDetail = CommanListToDataTableConverter.ConvertToDataTable(osm.OutwardDetailModel);
+            DataView dv = new DataView(dtOutDetail);
+            DataTable dtn = dv.ToTable(false, "DeliveryOrderID", "ProductID", "InwardDID", "LotNO", "DOQuantity", "InwardLocationID", "StorageAreaID");
+            DataSet ds = new DataSet();
+            using (SqlConnection connection = new SqlConnection(connectionstring))
             {
-                var data = _context.Outward_Insert(obj.outwardID,obj.warehouseID,obj.outWardDate,obj.billStartDate,obj.deliveryOrderID,
-                    obj.customerPartyID,obj.truckNo,obj.containerNo,obj.transporterName,obj.remarks,
-                    obj.createdBy,obj.customerID,obj.driverName,obj.driverNo,obj.docID,obj.loadingBy,obj.transferID,obj.dispatchID);
-                
-                return Ok(data);
+                using (SqlCommand command = new SqlCommand("DeliveryOrder_Insert", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter[] param = new SqlParameter[16];
+                    param[0] = new SqlParameter("@OutwardID", osm.OutwardID);
+                    param[1] = new SqlParameter("@WarehouseID", Convert.ToInt32(osm.WarehouseID));
+                    param[2] = new SqlParameter("@OutWardDate", Convert.ToDateTime(osm.OutWardDate));
+                    param[3] = new SqlParameter("@BillStartDate", Convert.ToDateTime(osm.OutWardDate));
+                    param[4] = new SqlParameter("@DeliveryOrderID", osm.DeliveryOrderID);
+                    param[5] = new SqlParameter("@CustomerPartyID", osm.CustomerPartyID);
+                    param[6] = new SqlParameter("@TruckNo", Convert.ToString(osm.TruckNo));
+                    param[7] = new SqlParameter("@ContainerNo", osm.ContainerNo);
+                    param[8] = new SqlParameter("@TransporterName", osm.TransporterName);
+                    param[9] = new SqlParameter("@Remarks", osm.Remarks);
+                    param[10] = new SqlParameter("@CreatedBy", osm.CreatedBy);
+                    param[11] = new SqlParameter("@CustomerID", Convert.ToInt32(osm.CustomerID));
+                    param[12] = new SqlParameter("@TD_OutwardDetail", dtn);
+                    param[13] = new SqlParameter("@TD_OutwardCharges", Convert.ToBoolean(osm.OutwardChargeModel));
+                    param[14] = new SqlParameter("@DriverName", osm.DriverName);
+                    param[15] = new SqlParameter("@DriverNo", osm.DriverNo);
+                    param[16] = new SqlParameter("@DocID", osm.DocID);
+                    param[17] = new SqlParameter("@LoadingBy", osm.LoadingBy);
+                    param[18] = new SqlParameter("@TransferID", osm.TransferID);
+                    param[19] = new SqlParameter("@DispatchID", osm.DispatchID);
+                    command.Parameters.AddRange(param);
+                    connection.Open();
+                    using (SqlDataAdapter da = new SqlDataAdapter(command))
+                    {
+                        da.Fill(ds);
+                    }
+                    connection.Close();
+                }
             }
-            catch (System.Exception)
-            {
-
-                return BadRequest();
-            }
+            var data = ds;
+            return Ok(data);
         }
+        //public IHttpActionResult Outward_Insert(cls_Outward obj)
+        //{
+        //    try
+        //    {
+        //        var data = _context.Outward_Insert(obj.outwardID,obj.warehouseID,obj.outWardDate,obj.billStartDate,obj.deliveryOrderID,
+        //            obj.customerPartyID,obj.truckNo,obj.containerNo,obj.transporterName,obj.remarks,
+        //            obj.createdBy,obj.customerID,obj.driverName,obj.driverNo,obj.docID,obj.loadingBy,obj.transferID,obj.dispatchID);                
+        //        return Ok(data);
+        //    }
+        //    catch (System.Exception)
+        //    {
+        //        return BadRequest();
+        //    }
+        //}
 
         [HttpPost]
         [Route("api/Outward/GetDeliveryOrderByID")]
---
+++ WMS_WebAPI/WMS_WebAPI/Models/OutwardSaveModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WMS_WebAPI.Models.Context;
+
+namespace WMS_WebAPI.Models
+{
+    public class OutwardSaveModel
+    {
+        public Nullable<int> OutwardID { get; set; }
+        public Nullable<int> WarehouseID { get; set; }
+        public Nullable<DateTime> OutWardDate { get; set; }
+        public Nullable<DateTime> BillStartDate { get; set; }
+        public Nullable<int> DeliveryOrderID { get; set; }
+        public Nullable<int> CustomerPartyID { get; set; }
+        public string TruckNo { get; set; }
+        public string ContainerNo { get; set; }
+        public string TransporterName { get; set; }
+        public string Remarks { get; set; }
+        public Nullable<int> CreatedBy { get; set; }
+        public Nullable<int> CustomerID { get; set; }
+        public string DriverName { get; set; }
+        public string DriverNo { get; set; }
+        public Nullable<int> DocID { get; set; }
+        public Nullable<int> LoadingBy { get; set; }
+        public Nullable<int> TransferID { get; set; }
+        public Nullable<int> DispatchID { get; set; }
+        public List<OutwardDetail> OutwardDetailModel { get; set; }
+        public List<OutwardCharge> OutwardChargeModel { get; set; }
+    }
+}
