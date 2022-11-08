3d71edb8ebd1a871bfb3c9715edd31911c40cea6
dharmendra <dharmendra.bhadoria@apaarinfosystems.com>
2022-11-08 11:38:16 +0530

    done

---
+++ WMS_WebAPI/WMS_WebAPI/Controllers/FileUplaodController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WMS_WebAPI.Controllers
+{
+    
+    public class FileUplaodController : ApiController
+    {
+        // GET api/<controller>
+        [HttpPost]
+        [Route("api/FileUplaod/Upload")]
+        
+        public Task<IActionResult> Upload(HttpPostedFileBase file)
+        {
+            //......
+        }
+
+        [HttpPost]
+        [Route("api/FileUplaod/Upload1")]
+
+        public Task<IActionResult> Upload1()
+        {
+            //......
+        }
+    }
+}
