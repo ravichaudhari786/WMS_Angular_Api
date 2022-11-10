4e1f98916a9adf7e1eb0680386618c31dc750ff1
dharmendra <dharmendra.bhadoria@apaarinfosystems.com>
2022-11-10 18:25:11 +0530

    add  save file  using binary

---
+++ WMS_WebAPI/WMS_WebAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WMS_WebAPI.Models;
+using System.Data;
+
+namespace WMS_WebAPI.Controllers
+{
+   
+
+    public class FileUploadController : ApiController
+    {
+        [HttpPost]
+        [Route("api/FileUpload/FileUplaodInfo")]
+        public IHttpActionResult FileUplaodInfo(FileInputData obj)
+        {
+           
+
+            Cls_FileUpload obj1 = new Cls_FileUpload();
+            // obj1.info(obj);
+
+
+            string path = System.Configuration.ConfigurationManager.AppSettings["FileUploadPath"].ToString() + obj.name;
+            obj1.Base64ToImage(obj).Save(path);
+            FileRes fro = new FileRes();
+            fro.FileName = obj.name;
+            fro.FilePath = path;
+
+            return Ok(fro);
+        }
+       
+    }
+   
+}
---
+++ WMS_WebAPI/WMS_WebAPI/Models/Cls_FileUpload.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using WMS_WebAPI.Controllers;
+
+namespace WMS_WebAPI.Models
+{
+    public class Cls_FileUpload
+    {
+        public  Boolean info(FileInputData obj)
+        {
+            string[] s = obj.base64.ToString().Split(',');
+            byte[] imageBytes = Convert.FromBase64String(s[1]);
+
+            string path = System.Configuration.ConfigurationManager.AppSettings["FileUploadPath"].ToString() + obj.name;
+            File.WriteAllBytes(path, imageBytes);
+            return true;
+        }
+
+        public System.Drawing.Image Base64ToImage(FileInputData obj)
+        {
+            string[] s = obj.base64.ToString().Split(',');
+            byte[] imageBytes = Convert.FromBase64String(s[1]);
+            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+            return image;
+        }
+    }
+
+    public class FileInputData
+    {
+        public string base64 { get; set; }
+
+
+        public string name { get; set; }
+
+        public Nullable<int> size { get; set; }
+
+        public string type { get; set; }
+
+    }
+
+    public  class FileRes
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
